780e4f58877e73c4c16a5cb202b6ae6895a9328f
Odilon Corrêa <odilon.correa@gmail.com>
2025-07-09 09:05:27 -0300

    Teste - Frontend

--- src/main/java/com/cefet/ds_guia12/config/SecurityConfig.java
+++ src/main/java/com/cefet/ds_guia12/config/SecurityConfig.java
@@ -118,7 +118,7 @@ public class SecurityConfig {
 	      @Override
 	      public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	          .allowedOrigins("http://localhost:4200")
+	          .allowedOrigins("http://localhost:4200", "https://ds-guia12.netlify.app")
 	          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	          .allowedHeaders("*")
 	          .allowCredentials(true);
